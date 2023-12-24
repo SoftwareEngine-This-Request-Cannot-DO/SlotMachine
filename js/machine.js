1712d3922d943f7825822f7de53c83cc734f955c
wwwzzzzxss <s9803179434@email.com>
2023-12-24 20:06:03 +0800

    first commit

--- js/machine.js
+++ js/machine.js
@@ -1,37 +1,45 @@
 
+
+var is_reel = false;
+var magnificat = [1000,100,100,10,10,10,3,3,3];
 document.addEventListener("DOMContentLoaded", function(event) {
     const betButton = document.getElementById("bet-button");
-    const creditsDisplay = document.getElementById("credits-display");
-    const isBalance = document.getElementById("is_balance");
-
-
-    let credits = 10000;
-    var win = false;
-
+    let credits = 10000; //我的點數
     betButton.addEventListener("click", function() {
-        // 获取下注金额
+        console.log("數字是",winNumber);
         const betAmount = parseInt(document.getElementById("bet-input").value);
-        // 检查是否有足够的信用点数下注
         if (betAmount > credits) {
             alert("餘額不足！");
-            isBalance.textContent = "NO BALANCE";
         }
-        else if(betAmount <= 100 ){
+        else if(betAmount <= 100 || isNaN(betAmount)){
             alert("下注過少");
         }
-        else {
-            if(!is_roll){
-                rollAll();
-                credits -= betAmount;
-                document.getElementById("credits-display").textContent = credits ;
-                document.getElementById("is_balance").textContent = winCls;
-                
-                if(winCls == "win2"){
-                    document.getElementById("jackpot").textContent = betAmount * 5 //倍率看圖案
-                }       
-            }           
+        else {             
+            rollAll();
+            credits -= betAmount;
+            document.getElementById("credits-display").textContent = credits ;
+            document.getElementById("is_balance").textContent = betAmount;           
+            if(winNumber != -1){
+                credits += betAmount * magnificat[winNumber]
+                document.getElementById("credits-display").textContent = credits;
+                document.getElementById("jackpot").textContent = betAmount * magnificat[winNumber] //倍率看圖案
+            } 
+            console.log("中?水果",winNumber);     
         }
-
     });
-
 });
+
+/*
+function is_spin(){
+    document.getElementById("bet-button").disabled = true;
+    
+    // 兩秒後再次啟用按鈕
+    setTimeout(function() {
+        document.getElementById("bet-button").disabled = false;
+    }, 4000);
+}*/
+
+
+
+
+
--- js/reel.js
+++ js/reel.js
@@ -1,5 +1,4 @@
 //中獎設定
-var magnificat = [1000,100,100,10,10,10,3,3,3];
 var probabilityArr = [0.5,1.5,1.5,8,8,8,10,10,10];
 function setProbability(){
   var randomNum = Math.random();
@@ -25,33 +24,20 @@ function setProbability(){
 }
 
 const debugEl = document.getElementById('debug'),
-  // Mapping of indexes to icons: start from banana in middle of initial position and then upwards
   iconMap = ["banana", "seven", "cherry", "plum", "orange", "bell", "bar", "lemon", "melon"];
   let tolProbabilty = 1;
   const random_number = Math.random() ;
   let probability = [];
-  
   let money = [];
- 
-  // Width of the icons
   icon_width = 79,
-  // Height of one icon in the strip
   icon_height = 79,
-  // Number of icons in the strip
   num_icons = 9,
-  // Max-speed in ms for animating one icon down
   time_per_icon = 100,
-  // Holds icon indexes
   indexes = [0, 0, 0];
 
 
-/** 
- * Roll one reel
- */
-var resetPos = [0,0,0],winNumber = -1,failNumber = -1;
-
-var count = -1,win = -1,closeProbablity = true;
-
+var resetPos = [0,0,0], failNumber = -1,notWin,winNumber = -1;
+var count = -1,closeProbablity = true;
 const roll = (reel, offset = 0) => {
   count++;
   if(count % 3 == 0){ 
@@ -68,7 +54,7 @@ const roll = (reel, offset = 0) => {
     failNumber = 0;
     resetPos[count % 3 ] %= 9;
     if(resetPos[count % 3] > winNumber){
-      cycle = 9 - (resetPos[count % 3] - winNumber);
+      cycle = 9 - (resetPos[count % 3] - winNumber); 
     }
     else{
       cycle = winNumber - resetPos[count % 3];
@@ -79,22 +65,26 @@ const roll = (reel, offset = 0) => {
     cycle = 0;
     failNumber = Math.round(Math.random() * num_icons); 
     resetPos[count % 3] += failNumber;
-   // console.log("resetpos", resetPos[count % 3]);
+    notWin = resetPos[0] % 9; //確保不會中獎
+    if(count % 3 == 2 && resetPos[2] == notWin){ 
+      if ((notWin + 1) == 9){  
+        failNumber -= 2;  
+        resetPos[count % 3] -= 2;
+      }
+      else{
+        failNumber += 1;  
+        resetPos[count % 3] += 1;
+      }
+    }
   }
 
-  const delta = failNumber + cycle;
+  const delta =  ((offset + 2) * num_icons) + failNumber + cycle;
   console.log("delta",delta);
-  //((offset + 2) * num_icons)
 
-
-  // Return promise so we can wait for all reels to finish
   return new Promise((resolve, reject) => {
     const style = getComputedStyle(reel),
-      // Current background position
       backgroundPositionY = parseFloat(style["background-position-y"]),
-      // Target background position
       targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
-    
       normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
 
   setTimeout(() => {
@@ -103,55 +93,38 @@ const roll = (reel, offset = 0) => {
       reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
     }, offset * 150);
 
-    // After animation
     setTimeout(() => {
-      // Reset position, so that it doesn't get higher without limit
       reel.style.transition = `none`;
       reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
-      // Resolve this promise
       resolve(delta % num_icons);
     }, (8 + 1 * delta) * time_per_icon + offset * 150);  
   });
-
 };
 
-/**
- * Roll all reels, when promise resolves roll again
- */
-let is_roll = false;
+
 let winCls = "";
 function rollAll() {
-  if(is_roll){
-    return;
-  }
-  else{
-    is_roll = true;
-  }
-
   debugEl.textContent = 'rolling...';
   const reelsList = document.querySelectorAll('.slots > .reel');
   Promise
-
-  // Activate each reel, must convert NodeList to Array for this with spread operator
   .all([...reelsList].map((reel, i) => roll(reel, i)))
 
-  // When all reels done animating (all promises solve)
   .then(deltas => {
-    // add up indexes
     deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta) % num_icons);
     debugEl.textContent = indexes.map(i => iconMap[i]).join(' - ');
-    is_roll = false;
-    // Win conditions
-    if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
-      winCls = indexes[0] == indexes[2] ? "win2" : "win1" ;
+    if (indexes[0] == indexes[1] && indexes[1] == indexes[2]) {
+      winCls = "win2" ;
       document.querySelector(".slots").classList.add(winCls);
-      
       setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000);
     }
-
-    setTimeout(rollAll, 500);
   });
 }
 ;
-
 setTimeout(rollAll, 500);
+
+
+
+
+
+
+
